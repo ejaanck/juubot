4e9c6b88c791e347bc0961685976d34afbfc5efe
ejaanck <149357949+ejaanck@users.noreply.github.com>
2024-01-02 11:16:09 +0700

    Update ping.py

--- AyiinXd/modules/ping.py
+++ AyiinXd/modules/ping.py
@@ -51,20 +51,20 @@ async def _(ping):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
     Ayiin = await edit_or_reply(ping, "**✩**")
-    await Ayiin.edit("**㋡㋡**")
-    await Ayiin.edit("**㋡㋡㋡**")
-    await Ayiin.edit("**㋡㋡㋡㋡**")
-    await Ayiin.edit("**㋡㋡㋡㋡㋡**")
+    await Ayiin.edit("**⚡**")
+    await Ayiin.edit("**⚡⚡**")
+    await Ayiin.edit("**⚡⚡⚡**")
+    await Ayiin.edit("**⚡⚡⚡⚡**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     user = await ping.client.get_me()
     await Ayiin.edit("🥵")
     sleep(3)
     await Ayiin.edit(
-        f"**㋡ 𝙺𝙰𝚉𝚄-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ㋡**\n\n"
-        f"㋡ **𝙿𝙸𝙽𝙶𝙴𝚁 :** `%sms`\n"
-        f"㋡ **𝚄𝙿𝚃𝙸𝙼𝙴 :** `{uptime}` \n"
-        f"㋡ **𝙾𝚆𝙽𝙴𝚁 :** [{user.first_name}](tg://user?id={user.id})" % (duration)
+        f"**⚡ AHH YAMETE 🤤 ㋡**\n\n"
+        f"📶 **𝙿𝙸𝙽𝙶𝙴𝚁 :** `%sms`\n"
+        f"⏰ **𝚄𝙿𝚃𝙸𝙼𝙴 :** `{uptime}` \n"
+        f"🤩 **𝙾𝚆𝙽𝙴𝚁 :** [{user.first_name}](tg://user?id={user.id})" % (duration)
     )
 
 
@@ -123,7 +123,7 @@ async def _(pong):
 # .keping & kping Coded by Koala
 
 
-@ayiin_cmd(pattern=r"kazu$")
+@ayiin_cmd(pattern=r"juu$")
 async def _(pong):
     uptime = await get_readable_time((time.time() - StartTime))
     start = datetime.now()
@@ -145,14 +145,14 @@ async def _(pong):
     await kping.edit("8===✊D")
     await kping.edit("8===✊D💦")
     await kping.edit("8====D💦💦")
-    await kping.edit("**KAZUUUUU**")
-    await kping.edit("**DISINI KAZU YAGESYA.....**")
+    await kping.edit("**NGENTOT**")
+    await kping.edit("**HAI ANAK NGENTOT.....**")
     end = datetime.now()
     duration = (end - start).microseconds / 1000
     await kping.edit("🥵")
     sleep(3)
     await kping.edit(
-        f"**𝙺𝙰𝚉𝚄 𝚃𝙰𝙼𝙿𝙰𝙽!! 🥵**\n**𝙿𝙸𝙽𝙶𝙴𝚁** : %sms\n**𝙱𝙾𝚃 𝚄𝙿𝚃𝙸𝙼𝙴** : {uptime}🕛" % (duration)
+        f"**MUKA LU KEK TAI!! 🥵**\n**𝙿𝙸𝙽𝙶𝙴𝚁** : %sms\n**𝙱𝙾𝚃 𝚄𝙿𝚃𝙸𝙼𝙴** : {uptime}🕛" % (duration)
     )
 
 
